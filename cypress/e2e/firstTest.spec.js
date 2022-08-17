70227bd96fd84af610a13a0f2795422d3ab47dfa
Ian Hutchinson <ianhutch90@gmail.com>
2022-08-17 17:43:42 +0100

    Wroking with web tables

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -257,6 +257,7 @@ describe('Our first suite', () => {
         cy.contains('Tables & Data').click()
         cy.contains('Smart Table').click()
         
+        // example 1
         // find table body first, next find table row contain string 'Larry'
         cy.get('tbody').contains('tr', 'Larry').then( tableRow => {
             // activate the edit row (button)
@@ -270,10 +271,60 @@ describe('Our first suite', () => {
 
         })
 
-        // 
+        // example 2
+        cy.get('thead').find('.nb-plus').click()
+        // find the index of the row and find first & last name
+        cy.get('thead').find('tr').eq(2).then( tableRow => {
+            cy.wrap(tableRow).find('[placeholder="First Name"]').type('Ian')
+            cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Hutchinson')
+            // click checkmark to confirm changes
+            cy.wrap(tableRow).find('.nb-checkmark').click()
+            // verify the first/last name fields have been updated correctly
+            cy.get('tbody tr').first().find('td').then( tableColumns => {
+                cy.wrap(tableColumns).eq(2).should('contain', 'Ian')
+                cy.wrap(tableColumns).eq(3).should('contain', 'Hutchinson')
+            })
+        })
 
+         // example 3 - verify search functionality
+         cy.get('thead [placeholder="Age"]').type('20')
+         // add delay to give UI time to update the table before next step is started
+         cy.wait(500)
+         // find all table rows & check last column to verify the value 20 exists
+         cy.get('tbody tr').each( tableRow => {
+            cy.wrap(tableRow).find('td').eq(6).should('contain', 20)
+         })
         
+        //  exmaple 4 - view many results in a table
+        const age = [20, 30, 40, 200]
 
+        // cy.wrap(age).each( age => {
+        //     cy.get('thead [placeholder="Age"]').clear().type(age)
+        //     // add delay to give UI time to update the table before next step is started
+        //     cy.wait(500)
+        //     // find all table rows & check last column to verify the value 20 exists
+        //     cy.get('tbody tr').each( tableRow => {
+        //         cy.wrap(tableRow).find('td').eq(6).should('contain', age)
+        //     })
+        // })
+
+        // example 5 - find a persons age that does not exist (no data found)
+        cy.wrap(age).each( age => {
+            cy.get('thead [placeholder="Age"]').clear().type(age)
+            // add delay to give UI time to update the table before next step is started
+            cy.wait(500)
+            // find all table rows & check last column to verify the value 20 exists
+            cy.get('tbody tr').each( tableRow => {
+                if(age == 200) { // verify "no data found" message 
+                    cy.wrap(tableRow).should('contain', 'No data found')
+                } else {
+                    cy.wrap(tableRow).find('td').eq(6).should('contain', age)
+                }
+                
+            })
+        })
+
+         
 
     })
 
