b23eb1d13123c830fff58e89bf066310a4d6ddf6
Ian Hutchinson <ianhutch90@gmail.com>
2022-09-03 12:54:51 +0100

    Test to verify navigation across app using page objects pattern
    Using beforeEach to create method that will be executed for every
    test. Created a first page obj (navigation page obj) responsable
    navigating to each page.

---
+++ cypress/support/page_objects/navigationPage.js
@@ -0,0 +1,53 @@
+
+function selectGroupMenuItem(groupName){
+    cy.contains('a', groupName).then( menu => {
+        cy.wrap(menu).find('.expand-state g g').invoke('attr', 'data-name').then( attr => {
+            if( attr.includes('left'))
+                cy.wrap(menu).click()
+        })
+    })
+}
+
+
+export class NavigationPage{
+
+    formLayoutsPage(){
+        // cy.contains('Forms').click()
+        selectGroupMenuItem('Forms')
+        cy.contains('Form Layouts').click()
+    }
+
+    datePickerPage(){
+        // cy.contains('Forms').click()
+        selectGroupMenuItem('Forms')
+        cy.contains('Datepicker').click()
+    }
+
+    dialogPage(){
+        selectGroupMenuItem('Modal & Overlays')
+        cy.contains('Dialog').click()
+    }
+
+    toasterPage(){
+        selectGroupMenuItem('Modal & Overlays')
+        cy.contains('Toastr').click()
+    }
+
+    tooltipPage(){
+        selectGroupMenuItem('Modal & Overlays')
+        cy.contains('Tooltip').click()
+    }
+
+    smartTablePage(){
+        selectGroupMenuItem('Tables & Data')
+        cy.contains('Smart Table').click()
+    }
+
+    
+
+
+
+
+}
+
+export const navigateTo = new NavigationPage()
